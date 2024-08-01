53327f75f2d971954e96c9f19c6637f19d7a4dc5
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2024-08-01 20:56:32 +0530

    4

---
+++ arrays/easy/secondlarge.cpp
@@ -0,0 +1,25 @@
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int largest=arr[0];
+	    int second=-1;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]>largest)
+	        {
+	            second=largest;
+	            largest=arr[i];
+	        }
+	        else if(arr[i]<largest&&arr[i]>second)
+	        {
+	            second=arr[i];
+	        }
+	        
+	    }
+	    return second;
+	}
+};
