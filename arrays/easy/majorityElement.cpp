199a3775871778ac2f9a69ebdc0eef4c6c24e02d
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2024-08-03 13:41:36 +0530

    3

---
+++ arrays/easy/majorityElement.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int no;
+        
+        for(int i=0;i<n;i++)      
+          {
+              if(count==0)
+              {
+                  count++;
+                  no=nums[i];
+              }
+              else if(nums[i]==no)
+              count++;
+              else
+              count--;
+            }
+            int cnt=0;
+               for(int i=0;i<n;i++)    
+               {
+                   if(no==nums[i])
+                   cnt++;
+               }  
+               if(cnt>n/2)
+               return no;
+                return -1;
+        }
+        
+    
+};
