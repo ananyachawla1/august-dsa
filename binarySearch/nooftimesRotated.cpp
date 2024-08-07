f9f452653c75b32ea0045fd84f3b94d888ef91cc
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2024-08-07 14:39:25 +0530

    1

---
+++ binarySearch/nooftimesRotated.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int start=0,n=arr.size();
+        int end=n-1;
+        int mid;
+        while(start<=end)
+        {
+            mid=(start+end)/2;
+            if(arr[mid]<arr[0])
+            end=mid-1;
+            else start=mid+1;
+        }
+        return start%n;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
